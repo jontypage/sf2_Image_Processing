9900ce1b42d3adbee97d63ad58e618d9343840bd
jontypage <jontypage@gmail.com>
2019-05-20 18:20:19 +0100

    More Part 2 - start of LBT

---
+++ lbt_opt_s.m
@@ -0,0 +1,21 @@
+function opt = lbt_opt_s(X, step, N)
+s = 1;
+for i = 1:100
+    steps = lbt_opt_step(X, step, N, s);
+    Yq = lbt_f(X, N, s, steps);
+    Yr = regroup(Yq,N)/N;
+    bits = dctbpp(Yr, N);
+    if i == 1
+        opt = s;
+        opt_bits = bits;
+    else
+        if bits < opt_bits
+            opt = s;
+            opt_bits = bits;
+        end
+    end
+    s = s + 0.01;
+    disp(s)
+    disp(opt_bits)
+end
+return
